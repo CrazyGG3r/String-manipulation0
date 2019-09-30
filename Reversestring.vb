692686318e6d09d0645c39e976e0baaf3d0a1556
Shaheer Ul Islam <54592666+CrazyGG3r@users.noreply.github.com>
2019-09-30 23:17:45 +0500

    String Reverse.
    String manipulation :  Altering String to its reverse form.

---
+++ Reversestring.vb
@@ -0,0 +1,28 @@
+﻿Module Reversestring
+
+    Sub Main()
+        Dim inputstring As String
+        Dim outstring As String
+        Dim a As String
+        Dim count, count1, supercount As Integer
+        Console.WriteLine("Welcome to String Reversinator")
+        Console.WriteLine("how many times do you want to do this? :")
+        supercount = Console.ReadLine
+        inputstring = ""
+        count = 0
+        a = ""
+        For count = 1 To supercount
+            outstring = ""
+            Console.WriteLine("Enter your string to be reversed :")
+            inputstring = Console.ReadLine
+            For count1 = Len(inputstring) To 1 Step -1
+                a = Mid(inputstring, count1, 1)
+                outstring = outstring & a
+            Next
+            Console.WriteLine("Your string reversed:" & outstring)
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module

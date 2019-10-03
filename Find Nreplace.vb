d4ec08158b32aa1cb87b1196c91affaee984a15c
Shaheer Ul Islam <54592666+CrazyGG3r@users.noreply.github.com>
2019-10-03 16:01:49 +0500

    Find N replace
    Finding a character in a string and then replacing it.

---
+++ Find Nreplace.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim instr, again, Charreplace, charfind, charcontain, outstring As String
+        Dim count, c As Integer
+
+        Console.WriteLine("Howmany times do you want to do this?")
+        count = Console.ReadLine
+        c = 1
+        again = "Y"
+        While c <> count Or again = "Y"
+            Charreplace = ""
+            charcontain = ""
+            charfind = ""
+            instr = ""
+            outstring = ""
+
+            Console.WriteLine("Enter text :")
+            instr = Console.ReadLine
+            Console.WriteLine("Enter character to find :")
+            charfind = Console.ReadLine
+            Console.WriteLine("enter character to replace with :")
+            Charreplace = Console.ReadLine
+
+            For a As Integer = 1 To Len(instr)
+                charcontain = Mid(instr, a, 1)
+                If Asc(charcontain) = Asc(charfind) Then
+                    charcontain = Charreplace
+                End If
+                outstring = outstring & charcontain
+            Next
+            Console.WriteLine(outstring)
+            Console.ReadKey()
+            Console.Write("again? (Y/N):")
+            again = Console.ReadLine
+            UCase(again)
+            c = c + 1
+        End While
+
+
+    End Sub
+
+End Module
